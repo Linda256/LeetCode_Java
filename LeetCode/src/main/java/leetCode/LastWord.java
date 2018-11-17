62013eec456e16b004d0d565a9de9c6e4045718c
Linda Wang <lindawang256@gmail.com>
2018-11-16 17:15:13 -0800

    Length of last word

---
+++ LeetCode/src/main/java/leetCode/LastWord.java
@@ -0,0 +1,25 @@
+package leetCode;
+/*
+ * Given a string s consists of upper/lower-case alphabets 
+ * and empty space characters ' ', return the length of last
+ *  word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of 
+non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+ */
+
+public class LastWord {
+	public int lengthOfLastWord(String s) { 
+        String st= s.trim();
+        int start=st.lastIndexOf(" ");
+        return st.substring(start+1).length();
+        
+    }	
+}
