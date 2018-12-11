b2de69c0e67d9adbec36db678be87c9c000e89b2
Linda Wang <lindawang256@gmail.com>
2018-12-11 09:54:38 -0800

    954 improve performance using HashMap

--- LeetCode/src/main/java/leetCode/DoubleNumArray.java
+++ LeetCode/src/main/java/leetCode/DoubleNumArray.java
@@ -1,8 +1,11 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DoubleNumArray {
 	public boolean canReorderDoubled(int[] a) {
@@ -28,11 +31,95 @@ public class DoubleNumArray {
        return true; 
         
     }
+	
+	public boolean canReorderDoubled2(int[] a) {
+
+		Arrays.sort(a);
+		Map<Integer, Integer> map=new HashMap<>();
+		for (int n : a){
+			if(!map.containsKey(n)){
+				map.put(n, 1);
+			}
+			else{
+				map.put(n, map.get(n)+1);
+			}
+		}
+		System.out.print(map);
+		
+
+		for (int n: a){
+			if (!map.containsKey(n)) continue;
+			if (n==0){
+				if (map.get(n)%2!=0) return false;
+				else map.remove(n);
+			} else{
+				int m = n>0 ? 2*n : n/2;
+				if (!map.containsKey(m)) return false;
+			      else {
+			    	  if((map.get(m)-1) == 0) map.remove(m);
+			    	  else map.put(m,map.get(m)-1);
+			    	  if((map.get(n)-1) == 0) map.remove(n);
+			    	  else map.put(n,map.get(n)-1);
+			    	 
+					}
+			}
+	  	
+		}
+		
+			
+			//System.out.println("m "+m);
+		System.out.println("size: "+map.size());
+		return map.size()==0;
+		 
+	}
+	
+	public boolean canReorderDoubled3(int[] a) {
+		Arrays.sort(a);
+		Map<Integer,Integer> map = new HashMap<>();
+			for (int n:a){
+				if (n==0){
+					if(map.containsKey(0)) {
+						map.remove(0);
+					}
+					else map.put(n, 1);
+					continue;
+				}
+				int m;
+				if(n<0){
+					m= 2*n;
+				}
+				else{
+					if (n%2!=0){
+						if(!map.containsKey(n)) map.put(n, 1);
+						else map.put(n, map.get(n)+1);
+						continue;
+					}
+					else m=n/2;
+				}
+				if(!map.containsKey(m)){				
+					if (!map.containsKey(n)) map.put(n, 1);
+					else map.put(n, map.get(n)+1);
+				}
+				else{
+					if(map.get(m)==1) map.remove(m);
+					else map.put(m, map.get(m)-1);
+				}
+		
+			}
+		System.out.println(map);
+		System.out.println("size: "+map.size());
+		return map.size()==0;	
+	}
 
 	public static void main(String[] args) {
 		DoubleNumArray dn = new DoubleNumArray();
-		int[] input = {1,2,4,16,8,4};
-		boolean res=dn.canReorderDoubled(input);
+		//int[] input = {2,2,4,5,-8,-4};
+		//int[] input ={4,-2,2,-4,0,0};
+		//int[] input ={3,3,1,6};
+		//int [] input ={4,-4,4,-2,-4,-8};
+		//int[] input={1,1,1,1,2,2,2,2};
+		int[] input={0,0};
+		boolean res=dn.canReorderDoubled3(input);
 		System.out.println(res);
 
 	}
